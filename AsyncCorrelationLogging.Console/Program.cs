947e7f4fd61e440eecc65b37b62ee8f33e2bcdf3
Martin Pöpel <martin.poepel@5minds.de>
2018-12-19 18:44:47 +0100

    Add first classes

---
+++ AsyncCorrelationLogging.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AsyncCorrelationLogging.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Logger logger = new Logger();
+            logger.log("Test");
+        }
+    }
+}
